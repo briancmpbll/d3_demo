c86af419b70fea9af4644e693e72d185f933747f
Brian Campbell <briancmpbll@gmail.com>
2016-08-21 16:52:53 -0700

    Initial graph display

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = (grunt) => {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      files: ['Gruntfile.js', 'app/**/*.js', 'src/javascripts/*.js']
+    },
+    less: {
+      development: {
+        files: {
+          'public/css/main.css': 'src/less/main.less'
+        }
+      }
+    },
+    watch: {
+      jshint: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
+      less: {
+        files: ['src/less/**/*.less'],
+        tasks: ['less']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['jshint', 'less', 'watch']);
+};
